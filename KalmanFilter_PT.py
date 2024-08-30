71d0bfb15ed6762b3640e303afbcd58ce428c2bf
ryan-doan <doanngocdung01@gmail.com>
2024-08-29 21:34:45 -0400

    wrap the filter to let us pass x into forward
    Being able to pass x in the forward pass is potentially necessary for lirpa to generate the network's graph

--- KalmanFilter_PT.py
+++ KalmanFilter_PT.py
@@ -6,9 +6,9 @@ from numpy import isscalar
 from copy import deepcopy
 from filterpy.common import reshape_z
 
-class KalmanFilter(nn.Module):
+class KalmanFilterModule(nn.Module):
     def __init__(self, dim_x, dim_z, dim_u=0):
-        super(KalmanFilter, self).__init__()
+        super(KalmanFilterModule, self).__init__()
         if dim_x < 1:
             raise ValueError('dim_x must be 1 or greater')
         if dim_z < 1:
@@ -20,7 +20,6 @@ class KalmanFilter(nn.Module):
         self.dim_z = dim_z
         self.dim_u = dim_u
 
-        self.x = torch.zeros((dim_x, 1))
         self.P = torch.eye(dim_x)
         self.Q = torch.eye(dim_x)
         self.B = None
@@ -37,10 +36,7 @@ class KalmanFilter(nn.Module):
         self.SI = torch.zeros((dim_z, dim_z))
 
         self._I = torch.eye(dim_x)
-        self.x_prior = self.x.clone()
         self.P_prior = self.P.clone()
-
-        self.x_post = self.x.clone()
 
         self.P_post = self.P.clone()
 
@@ -50,10 +46,10 @@ class KalmanFilter(nn.Module):
 
         self.inv = torch.inverse
 
-    def forward(self):
-        return self.predict()
+    def forward(self, x):
+        return self.predict(x)
 
-    def predict(self, u=None, B=None, F=None, Q=None):
+    def predict(self, x, u=None, B=None, F=None, Q=None):
         if B is None:
             B = self.B
         if F is None:
@@ -64,16 +60,17 @@ class KalmanFilter(nn.Module):
             Q = torch.eye(self.dim_x) * Q
 
         if B is not None and u is not None:
-            self.x = torch.matmul(F, self.x) + torch.matmul(B, u)
+            x = nn.Parameter(torch.matmul(F, x) + torch.matmul(B, u))
         else:
             #print(F)
             #print(self.x)
-            self.x = torch.matmul(F, self.x)
+            x = nn.Parameter(torch.matmul(F, x))
 
         self.P = self._alpha_sq * torch.matmul(torch.matmul(F, self.P), torch.transpose(F, 0, 1)) + Q
 
-        self.x_prior = self.x.clone()
         self.P_prior = self.P.clone()
+
+        return x
 
     def update(self, z, R=None, H=None):
         self._log_likelihood = None
@@ -115,3 +112,17 @@ class KalmanFilter(nn.Module):
         self.z = deepcopy(z)
         self.x_post = self.x.clone()
         self.P_post = self.P.clone()
+
+class KalmanFilter():
+    def __init__(self, dim_x, dim_z, dim_u=0):
+        self.x = nn.Parameter(torch.zeros((dim_x, 1)))
+        self.x_prior = self.x.clone()
+        self.x_post = self.x.clone()
+        self.model = KalmanFilterModule(dim_x, dim_z, dim_u)
+
+    def predict(self):
+        self.x, self.x_prior = self.model(self.x)
+        self.x_prior = self.x.clone()
+
+    def update(self, z, R=None, H=None):
+        self.model.update(z, R, H)
--- kf_test.py
+++ kf_test.py
@@ -1,6 +1,7 @@
 from KalmanFilter_PT import KalmanFilter
 import numpy as np
 import torch
+import auto_LiRPA
 
 def convert_bbox_to_z(bbox):
   """
@@ -15,24 +16,23 @@ def convert_bbox_to_z(bbox):
   s = w * h    #scale is just area
   r = w / float(h)
   #return tf.convert_to_tensor(np.array([x, y, s, r]).reshape((4, 1)), dtype=tf.float32)
-  z = torch.nn.Parameter(torch.tensor([[x],[y],[s],[r]]))
+  z = torch.nn.Parameter(torch.tensor([[x],[y],[s],[r]], requires_grad=True))
   return z.view(4, 1)
 
 bbox = np.array([1689.   ,  385.   , 1835.62 ,  717.71 ,   67.567])
 
 #define constant velocity model
 kf = KalmanFilter(dim_x=7, dim_z=4) 
-kf.F = torch.tensor(np.array([[1,0,0,0,1,0,0],[0,1,0,0,0,1,0],[0,0,1,0,0,0,1],[0,0,0,1,0,0,0],  [0,0,0,0,1,0,0],[0,0,0,0,0,1,0],[0,0,0,0,0,0,1]]))
-kf.H = torch.tensor(np.array([[1,0,0,0,0,0,0],[0,1,0,0,0,0,0],[0,0,1,0,0,0,0],[0,0,0,1,0,0,0]]))
+kf.model.F = torch.tensor(np.array([[1,0,0,0,1,0,0],[0,1,0,0,0,1,0],[0,0,1,0,0,0,1],[0,0,0,1,0,0,0],  [0,0,0,0,1,0,0],[0,0,0,0,0,1,0],[0,0,0,0,0,0,1]]))
+kf.model.H = torch.tensor(np.array([[1,0,0,0,0,0,0],[0,1,0,0,0,0,0],[0,0,1,0,0,0,0],[0,0,0,1,0,0,0]]))
 
-kf.R[2:,2:] = kf.R[2:,2:] * 10.
-kf.P[4:,4:] = kf.P[4:,4:] * 1000. #give high uncertainty to the unobservable initial velocities
-kf.P *= 10.
+kf.model.R[2:,2:] = kf.model.R[2:,2:] * 10.
+kf.model.P[4:,4:] = kf.model.P[4:,4:] * 1000. #give high uncertainty to the unobservable initial velocities
+kf.model.P *= 10.
 #self.kf.Q[-1,-1].assign(self.kf.Q[-1,-1] * 0.01)
 #self.kf.Q[4:,4:].assign(self.kf.Q[4:,4:] * 0.01)
-kf.Q[2,2] = 50
-kf.Q[-1,-1] = 50
+kf.model.Q[2,2] = 50
+kf.model.Q[-1,-1] = 50
 
-kf.x[:4] = convert_bbox_to_z(bbox)
-
-kf.predict()
+kf.x.data[:4] = convert_bbox_to_z(bbox)
+kf.model = auto_LiRPA.BoundedModule(kf.model, kf.x)
